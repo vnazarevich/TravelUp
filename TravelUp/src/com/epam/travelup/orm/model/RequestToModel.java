a4819057f63bd75bdd906ad76db0c1d38e2b43e1
serg1236 <seryoga.dah@gmail.com>
2015-01-31 14:59:53 +0200

    Моделька нової таблички

---
+++ TravelUp/src/com/epam/travelup/orm/model/RequestToModel.java
@@ -0,0 +1,42 @@
+package com.epam.travelup.orm.model;
+
+public class RequestToModel {
+	@DBField(name="id")
+	private int id;
+
+	@DBKey(name = "request_id")
+	private Tour request;
+
+	@DBKey(name = "model_id")
+	private Tour model;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Tour getRequest() {
+		return request;
+	}
+
+	public void setRequest(Tour request) {
+		this.request = request;
+	}
+
+	public Tour getModel() {
+		return model;
+	}
+
+	public void setModel(Tour model) {
+		this.model = model;
+	}
+
+	@Override
+	public String toString() {
+		return "RequestToModel [id=" + id + ", request=" + request + ", model="
+				+ model + "]";
+	}
+}
